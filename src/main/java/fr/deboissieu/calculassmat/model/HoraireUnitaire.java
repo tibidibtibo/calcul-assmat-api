7fbadb43505f5a960a4ecace7f93f9f7aed1cfb1
thibault.deboissieu <thibault.deboissieu@soprasteria.com>
2018-09-14 16:29:52 +0200

    [MAPPING] : + enum prénoms

--- src/main/java/fr/deboissieu/calculassmat/model/HoraireUnitaire.java
+++ src/main/java/fr/deboissieu/calculassmat/model/HoraireUnitaire.java
@@ -3,13 +3,14 @@ package fr.deboissieu.calculassmat.model;
 import java.util.Set;
 
 import fr.deboissieu.calculassmat.commons.excelfile.ActionHoraireEnum;
+import fr.deboissieu.calculassmat.commons.excelfile.PrenomEnum;
 import lombok.Getter;
 import lombok.Setter;
 
 public class HoraireUnitaire {
 	@Getter
 	@Setter
-	private Set<String> qui;
+	private Set<PrenomEnum> prenoms;
 
 	@Getter
 	@Setter
@@ -21,7 +22,7 @@ public class HoraireUnitaire {
 
 	public static HoraireUnitaire of(SaisieJournaliere saisieJournaliere) {
 		HoraireUnitaire horaireUnitaire = new HoraireUnitaire();
-		horaireUnitaire.setQui(saisieJournaliere.getQui());
+		horaireUnitaire.setPrenoms(PrenomEnum.fromPrenom(saisieJournaliere.getQui()));
 		horaireUnitaire.setAction(ActionHoraireEnum.fromAction(saisieJournaliere.getAction()));
 		horaireUnitaire.setHeureAction(saisieJournaliere.getHeureAction());
 		return horaireUnitaire;
