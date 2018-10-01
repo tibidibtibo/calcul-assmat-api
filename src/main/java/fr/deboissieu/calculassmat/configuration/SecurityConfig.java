311e8e2321c6b677ac3e3eac7a7db80d7d6d7f0e
Thibault DE BOISSIEU <thibault.deboissieu@soprasteria.com>
2018-10-01 16:59:41 +0200

    [SECURITY] : mise en place basic auth

--- src/main/java/fr/deboissieu/calculassmat/configuration/SecurityConfig.java
+++ src/main/java/fr/deboissieu/calculassmat/configuration/SecurityConfig.java
@@ -6,35 +6,24 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private static final String ENCODED_PASSWORD = "$2y$10$bQZ1oMnBrbFTtFwGLPPNne5YOQQvzwP8qTgRGWmpO7Y6JENTOThKG";
-
-	@Autowired
-	private AuthenticationEntryPoint authEntryPoint;
-
-	PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-				.anyRequest().authenticated()
-				.and().httpBasic()
-				.authenticationEntryPoint(authEntryPoint);
-	}
-
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-				.withUser("assmat")
-				.password(encoder.encode(ENCODED_PASSWORD))
-				.roles("api");
+				.withUser("assmat").password("{noop}assmat").roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				// .antMatchers("/", "/home").access("hasRole('USER')")
+				.antMatchers("/api/**").hasRole("USER")
+				.and()
+				.formLogin();
 	}
 
 }
