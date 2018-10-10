a1c6ddc32385fdf375d0e53d7aa82dae8f336397
Thibault DE BOISSIEU <thibault.deboissieu@soprasteria.com>
2018-10-10 13:47:45 +0200

    [SECURITY] : maj security + bcrypt

--- src/main/java/fr/deboissieu/calculassmat/configuration/SecurityConfig.java
+++ src/main/java/fr/deboissieu/calculassmat/configuration/SecurityConfig.java
@@ -1,26 +1,37 @@
 package fr.deboissieu.calculassmat.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Value(value = "${basic.auth.user}")
+	private String basicAuthUser;
+
+	@Value(value = "${basic.auth.encoded.password}")
+	private String basicAuthEncodedPassword;
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
 		auth.inMemoryAuthentication()
-				.withUser("assmat").password("{noop}assmat").roles("USER");
+				.passwordEncoder(new BCryptPasswordEncoder())
+				.withUser(basicAuthUser)
+				.password(basicAuthEncodedPassword)
+				.roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				// .antMatchers("/", "/home").access("hasRole('USER')")
 				.antMatchers("/**").hasRole("USER")
 				.and()
 				.formLogin();
