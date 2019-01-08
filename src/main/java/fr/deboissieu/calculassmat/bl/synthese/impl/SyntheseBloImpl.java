845dfa133ec93435aaa968617c60293e9c2479d6
Thibault <thibault@de-boissieu.fr>
2019-01-08 23:04:10 +0100

    [FIXME] : revoir calcul indemnités entretien

--- src/main/java/fr/deboissieu/calculassmat/bl/synthese/impl/SyntheseBloImpl.java
+++ src/main/java/fr/deboissieu/calculassmat/bl/synthese/impl/SyntheseBloImpl.java
@@ -193,6 +193,7 @@ public class SyntheseBloImpl implements SyntheseBlo {
 		return MathsUtils.roundTo2Digits(fraisRepas);
 	}
 
+	// FIXME : calcul faux !
 	private Double calculerIndemnitesEntretien(ParametrageEmploye employe, NombreHeures nbHeures) {
 
 		Integer nbJourInf = 0;
